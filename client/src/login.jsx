cd3747efa78b887ff957b54228ae442e2e8122b0
Arham097 <arhamhasan70@gmail.com>
2024-10-12 18:16:18 +0500

    login updated

--- client/src/login.jsx
+++ client/src/login.jsx
@@ -1,6 +1,9 @@
-const login=()=>{
-    console.log("login")
-    console.log("added login")
-    return<></>
-}
+import { useState } from "react";
+
+const login = () => {
+  const [email, setEmail] = useState("");
+  console.log("login");
+  console.log("added login");
+  return <></>;
+};
 export default login;
